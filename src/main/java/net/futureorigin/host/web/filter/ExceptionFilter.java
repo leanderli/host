ba1855d241039cc0dc17e0629cb58e5b5621fe71
leanderlee <23739932+leanderli@users.noreply.github.com>
2021-08-23 10:10:37 +0800

    optimize imports

--- src/main/java/net/futureorigin/host/web/filter/ExceptionFilter.java
+++ src/main/java/net/futureorigin/host/web/filter/ExceptionFilter.java
@@ -31,7 +31,7 @@ public class ExceptionFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
     }
 
     @Override
